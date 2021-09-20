9c7f9a0f9b839c6d265fb50331f6418ab7fdee37
Ajay Bharat Kolla <ajaybharat222@gmail.com>
2021-09-20 22:11:45 +0530

    Singleton pattern

---
+++ singletonPattern/Singleton_Pattern/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Singleton_Pattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Parallel.Invoke(
+                () => Printteacherinstance(),
+                () => Printstudentinstance()
+                );
+            Console.ReadLine();
+        }
+
+        private static void Printteacherinstance()
+        {
+            Singleton inst1 = Singleton.getInstance();
+            inst1.printFromInstance("from inst1");
+        }
+        private static void Printstudentinstance()
+        {
+            Singleton inst2 = Singleton.getInstance();
+            inst2.printFromInstance("from inst2");
+        }
+    }
+}
---
+++ singletonPattern/Singleton_Pattern/Singleton.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Singleton_Pattern
+{
+    public sealed class Singleton
+    {
+        private static int count = 0;
+        private static readonly object instancelock = new object();
+        private static Singleton instance = null;
+        public static Singleton getInstance()
+        {
+            if(instance == null)
+            {
+                lock(instancelock)
+                {
+                    if (instance == null)
+                        instance = new Singleton();
+                }
+            }
+            return instance;
+        }
+        private Singleton()
+        {
+            count++;
+            Console.WriteLine("counter value: " + count.ToString());
+        }
+
+        public void printFromInstance(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
